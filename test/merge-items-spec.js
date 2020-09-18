2ed9c0e755aff9f01a1d2c95599d0d324596e22b
Rhys Previte <previterp88@gmail.com>
2020-09-18 17:47:34 -0500

    Done save & show second test

--- test/merge-items-spec.js
+++ test/merge-items-spec.js
@@ -9,11 +9,42 @@ describe("The mergeItems function", () => {
     </table>
   `;
   it("should return no <tr>s and no <td>s for no items", () => {
-    expect.fail('please write this test');
+    //Arrange
+    let items = []; 
+
+    //Act
+    let result = mergeItems(template, items); 
+
+    //Assert
+    expect(result).to.include('<table>');
+    expect(result).to.include('</table>');
+    expect(result).to.include('<tbody>');
+    expect(result).to.include('</tbody>');
+    expect(result).to.not.include('<tr>');
+    expect(result).to.not.include('</tr>');
+    expect(result).to.not.include('<td>');
+    expect(result).to.not.include('</td>');
+    expect(result).to.not.include('<!-- Content here -->');
+
+    
   });
 
   it("should return a single <tr>, four <td>s, and a <form> for one uncompleted item", () => {
-    expect.fail('please write this test');
+    //Arrange
+    let items = [{title: "Title 1", category: "Category 1"}];
+    //Act
+    let result = mergeItems(template, items); 
+    //Assert 
+    expect(result).to.include('<table>');
+    expect(result).to.include('</table>');
+    expect(result).to.include('<tbody>');
+    expect(result).to.include('</tbody>');
+    expect(result).to.include('<tr>');
+    expect(result).to.include('</tr>');
+    expect(result).to.include('<td>Title 1</td>')
+    expect(result).to.include('<td>Category 1</td>')
+    expect(result).to.include('<form method="POST" action="/items/1">')
+    expect(result).to.not.include('<!-- Content here -->');
   });
 
   it("should return a single <tr>, four <td>s, and no <form> for one completed item", () => {
